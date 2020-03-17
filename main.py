e85714193fb1dc96383afd11321f997ba3aea6f3
Paulina <paulinakowalczykpoczta@gmail.com>
2020-03-17 14:49:33 +0100

    contributor added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 class Main:
     def __init__(self):
-        self.contributors = ["Przemysław Filipek", "Piotr Machnik"]
+        self.contributors = ["Przemysław Filipek", "Piotr Machnik", "Paulina Kowalczyk"]
 
     def __str__(self):
         return "Contributors list:\n{}".format("\n".join(self.contributors))
