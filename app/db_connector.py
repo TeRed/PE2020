6b986e6de0d907d82d06b20c418d66404b8f9753
Robert Łabuz <robsonik16@gmail.com>
2020-05-22 20:57:44 +0200

    SOLID fixes

--- app/db_connector.py
+++ app/db_connector.py
@@ -1,15 +1,35 @@
 import json
 from article import Article
+from abc import ABC, abstractmethod
+
+
+class DBio(ABC):
+    @abstractmethod
+    def read( db_path): ...
+
+    @abstractmethod
+    def write(articles, db_path): ...
+
+
+class DBioFile(DBio):
+    def read(db_path):
+        with open(db_path) as f:
+            load = json.load(f)
+        return load
+
+    def write(articles, db_path):
+        db_raw = [obj.__dict__ for obj in articles]
+        with open(db_path, 'w') as f:
+            json.dump(db_raw, f)
 
 
 class DBConnector:
-    def __init__(self, config_manager):
+    def __init__(self, config_manager,  database_io: DBio):
         self.config_manager = config_manager
+        self.DBio = database_io
 
     def read_json_file(self):
-        with open(self.config_manager.db_path) as f:
-            load = json.load(f)
-        return load
+        return self.DBio.read(self.config_manager.db_path)
 
     def get_all_articles(self):
         load = self.read_json_file()
@@ -38,16 +58,13 @@ class DBConnector:
     def add_article(self, obj):
         articles = self.get_all_articles()
         articles.append(obj)
+        self.DBio.write(articles, self.config_manager.db_path)
 
-        with open(self.config_manager.db_path, 'w') as f:
-            json.dump([obj.__dict__ for obj in articles], f)
 
     def remove_article_by_id(self, id):
         articles = self.get_all_articles()
         articles = [it for it in articles if it.id != id]
-
-        with open(self.config_manager.db_path, 'w') as f:
-            json.dump([obj.__dict__ for obj in articles], f)
+        self.DBio.write(articles, self.config_manager.db_path)
 
     def change_article_availability(self, id, available):
         articles = self.get_all_articles()
